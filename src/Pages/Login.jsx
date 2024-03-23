a81d7c1ca37ff2a0681203d0995b2dc9749e2585
Kanchan <kanchansingh30nov@gmail.com>
2024-03-23 16:40:48 +0530

    commit -1

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/LOGO2.png";
-import loginLogo from "../assets/LOG2.png";
-import { useNavigate } from "react-router-dom";
+import loginLogo from "../assets/log2.png"
+import {useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 import { doc, getDoc } from "firebase/firestore";
-import { auth, db } from "../firebase/firebase.config";
-import {  Modal } from 'flowbite-react';
 import { auth, db } from "../firebase/firebase.config";
 import {  Modal } from 'flowbite-react';
 
@@ -18,8 +16,10 @@ const Login = () => {
   const navigate = useNavigate();
   const { setUpRecaptcha } = useAuth();
   const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
+  
   
 
   const requestOtp = async (e) => {
@@ -53,37 +53,40 @@ const Login = () => {
       return;
     }
 
-   
-    try {
-      await result.confirm(otp);
-      const userRef = doc(db, "users", auth.currentUser.uid);
-      const userSnap = await getDoc(userRef);
-      if (userSnap.exists()) {
-        const userData = userSnap.data();
-        // console.log(`User DATA PROVIDED : ${JSON.stringify(userData)}`);
-        if (userData.userType === "agency-admin") {
-          navigate("/adminDashboard/dashboard");
-        } else if (userData.userType === "citizen") {
-          navigate("/CitizenDashboard");
-        } else {
-          console.error("Invalid user type");
-        }
+
+  try {
+    await result.confirm(otp);
+    const userRef = doc(db, "users", auth.currentUser.uid);
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) {
+      const userData = userSnap.data();
+      // console.log(`User DATA PROVIDED : ${JSON.stringify(userData)}`);
+      if (userData.userType === "agency-admin") {
+        navigate("/adminDashboard/dashboard");
+       
+      } else if (userData.userType === "citizen") {
+        navigate("/CitizenDashboard");
       } else {
-        console.error("User not found");
+        console.error("Invalid user type");
       }
-    } catch (error) {
-      console.error("Error with OTP confirmation:", error);
+    } else {
+      alert("User not found. Please sign up");
     }
-  };
+  } catch (error) {
+    alert("Incorrect OTP. Please try again");
+  } finally {
+      setLoading(false);
+  }
+
+};
+
 
   return (
     <div
       className=" bgLogin px-2  relative flex sm:flex-row flex-col  min-h-screen overflow-y-hidden h-screen pr-10  py-2 sm:py-8 bg-white "
-      className=" bgLogin px-2  relative flex sm:flex-row flex-col  min-h-screen overflow-y-hidden h-screen pr-10  py-2 sm:py-8 bg-white "
       id="login"
     >
       <Link to="/">
-      <div className=" flex pl-8  ">
       <div className=" flex pl-8  ">
           <img src={Logo} alt="Logo" className="h-8 w-8 mr-2 " />
           <span className="text-red-600 text-2xl font-medium">Crisis</span>
@@ -92,7 +95,7 @@ const Login = () => {
       </Link>
     
       <div className="grid place-items-center">
-        <div className="sm:grid place-items-center hidden pr-20 w-3/4">
+        <div className="sm:grid place-items-center hidden pr-20 w-full">
           <img src={loginLogo} />
         </div>
         <div className="space-y-4 pt-8">
@@ -108,26 +111,21 @@ const Login = () => {
 
 
       <div>
-       
-
-        <div className=" shadow-2xl m-8 px-8 py-12 rounded-2xl justify-center  flex items-center flex-col  w-auto mx-4   ">
-          <form
-            onSubmit={requestOtp}
-            className=" bg-gradient-to-r from-zinc-300 to-red-50 shadow-2xl  p-8 flex items-center justify-center"
-          >
+      
+     
+      {/* {loading ? <LoadingSpinner /> : null} */}
+        <div className=" bg-opacity-10  bg-slate-500  shadow-2xl  px-8 py-12 mt-24 ml-28  justify-center  flex items-center flex-col  w-auto mx-4   ">
+         
+          <form onSubmit={requestOtp} className="bg-gradient-to-r from-zinc-300 to-red-50 shadow-2xl  p-14 flex items-center justify-center" >
             {!isOtpRequested ? (
               <div>
                 {/* Your login form content goes here */}
-                <div className="grid place-items-center">
-                  <h1 className="text-4xl font-semibold space-y-6 mb-6 text-black">
-                    Login
-                  </h1>
-                </div>
-                {/* <div>
-                <img src={}/>
-               </div> */}
+               <div className="grid place-items-center">
+               <h1 className="text-4xl font-semibold space-y-6 mb-6 text-black">Login</h1>
+               </div>
                 <div className=" flex items-center justify-center text-semibold  font-medium ">
                   {" "}
+                  
                   <p className="text-black">Enter your Phone Number</p>
                 </div>
                 <div className="  flex items-center justify-center">
@@ -145,14 +143,21 @@ const Login = () => {
                     onClick={requestOtp}
                     type="submit"
                     className="bg-red-700 text-white font-medium px-4 py-2  rounded-md hover:bg-red-900"
+                    // disabled={loading}
                   >
+                     {/* {loading && (
+                    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50">
+                      <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-gray-900"></div>
+                    </div>
+                  )} */}
+                    
+                    {/* {loading ? "Loading..." : "Request OTP"} */}
                     Request OTP
                   </button>
                 </div>
               </div>
             ) : (
               <div className="flex flex-col font-medium ">
-                <div className="text-black">Enter the OTP:</div>
                 <div className="text-black">Enter the OTP:</div>
                 <div className="flex mb-4 pt-4">
                   <input
@@ -210,38 +215,12 @@ const Login = () => {
                 
               </Modal>
               </div>
-              Don't have an account. Please{" "}
-              <button
-                className="text-blue-700  underline   py-1 font-medium rounded-md "
-                onClick={() => setOpenModal(true)}
-              >
-                Sign-Up
-              </button>
-              <div >
-              <Modal show={openModal} onClose={() => setOpenModal(false)} className=" pt-52 px-96 w-full h-screen flex items-center justify-center bg-gray-900 bg-opacity-50">
-         
-                <Modal.Header ></Modal.Header>
-                
-                <Modal.Body className=" ">
-                  <div className="space-y-2 flex flex-col items-center justify-center text-center">
-                   
-                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                      Please Sign-up by your Phone
-                    </p>
-                    <p className="text-blue-800 font-medium">Link of the App</p>
-                  </div>
-                </Modal.Body>
-
-                <Modal.Footer>
-                 
-                </Modal.Footer>
-                
-              </Modal>
-              </div>
             </p>
           </div>
         </div>
+        
       </div>
+      
     </div>
   );
 };
--- src/components/AdminDashboard/AdminNavbar.jsx
+++ src/components/AdminDashboard/AdminNavbar.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Logo from "../../assets/LOGO2.png";
-import Avatar from "../../assets/ava.png";
+import Avatar from "../../assets/citizenAVa.jpg";
 import { doc, getDoc } from "firebase/firestore";
 import { auth, db } from "../../firebase/firebase.config";
 import search from '../../assets/search.png'
 import notify from '../../assets/notify.png'
-import down from '../../assets/down2.png'
-import up from '../../assets/up2.png'
+import down from '../../assets/down.png'
+import up from '../../assets/up.png'
 import { Modal } from 'flowbite-react';
 import { Link } from "react-router-dom";
 
@@ -16,7 +16,6 @@ const AdminNavbar = () => {
   const [user,setUser] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
   const [isClicked,setIsClicked] =useState(false);
-  const [openModal, setOpenModal] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
   const toggleDropdown = () => {
@@ -43,20 +42,18 @@ const AdminNavbar = () => {
   }, []);
   return (
 
-    <div className="bg-white  shadow-lg py-2   px-6   flex items-center justify-between  w-full relative">
-      <div className="flex items-center justify-center pl-4">
+    <div className=" bg-white py-1  pr-8 shadow-2xl flex items-center justify-between  w-full relative">
+      <div className="flex items-center pl-8">
         
         <img src={Logo} alt="Logo" className="h-8 w-8 mr-2 " />
         <span className="text-red-600 text-2xl font-medium">Crisis</span>
         <span className="text-red-600 font-semibold pt-2"> collab</span>
-       
       </div>
-      
       <div className="relative ">
       <input
         type="text"
         placeholder="Search..."
-        className="pl-11 w-96 pr-4 py-1 rounded-xl border-2  border-gray-300 focus:outline-none focus:border-gray-700"
+        className="pl-11 pr-4 py-1  w-96 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-black"
       />
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
        <img src={search}/>
@@ -64,7 +61,7 @@ const AdminNavbar = () => {
     </div>
    
       <div className="flex items-center justify-center">
-      <div className='w-5 h-5 mr-8  '>
+      <div className='w-7 h-7 mr-8  '>
        <img src={notify}/>
       </div>
         <div className="flex flex-col item-center justify-center">
@@ -75,15 +72,15 @@ const AdminNavbar = () => {
         </div>
         </div>
         <div>
-        <div className="relative flex pr-3">
+        <div className="relative flex">
         <img
               src={Avatar} // Replace with your avatar image
               alt="Avatar"
-              className="w-10 h-10 rounded-full border-black border"
+              className="w-10 h-10 rounded-full"
             />
           {/* Avatar */}
-          <button onClick={toggleDropdown} className="focus:outline-none flex space-x-1 pl-1 " >
-            {isClicked?  <img src={up} className=" mt-2"/>:<img src={down} className="mt-2"/>}
+          <button onClick={toggleDropdown} className="focus:outline-none flex space-x-1 pl-1" >
+            {isClicked?  <img src={up} className="mt-2"/>:<img src={down} className="mt-2"/>}
             
            
           </button>
@@ -92,11 +89,9 @@ const AdminNavbar = () => {
           {isOpen && (
             <div className="absolute z-50  top-12 right-0 flex flex-col justify-between items-center my-6 gap-x-3  bg-black rounded pl-8 pr-8">
               <a href="/adminDashboard/agency-profile" className="flex w-full items-center  my-1 px-8 py-2 mt-4 text-sm transition-all font-medium rounded-md text-white hover:bg-gray-700">
-            <div className="absolute z-50  top-12 right-0 flex flex-col justify-between items-center my-6 gap-x-3  bg-black rounded pl-8 pr-8">
-              <a href="/adminDashboard/agency-profile" className="flex w-full items-center  my-1 px-8 py-2 mt-4 text-sm transition-all font-medium rounded-md text-white hover:bg-gray-700">
                 Profile
               </a>
-              <a href="/adminDashboard/setting" className="flex w-full items-center my-1 px-8 py-2 text-sm transition-all font-medium rounded-md text-white hover:bg-gray-700">
+              <a href="#" className="flex w-full items-center my-1 px-8 py-2 text-sm transition-all font-medium rounded-md text-white hover:bg-gray-700">
                 Settings
               </a>
 
@@ -104,40 +99,7 @@ const AdminNavbar = () => {
                 className="  w-full items-center my-1 px-4- py-2 text-sm transition-all font-medium rounded-md text-white hover:bg-gray-700 "
                 onClick={() => setOpenModal(true)}
               >
-
-              <button
-                className="  w-full items-center my-1 px-4- py-2 text-sm transition-all font-medium rounded-md text-white hover:bg-gray-700 "
-                onClick={() => setOpenModal(true)}
-              >
                 Logout
-              </button>
-              <div >
-              <Modal show={openModal} onClose={() => setOpenModal(false)} className=" pt-52 px-96 w-full h-screen flex items-center justify-center bg-gray-900 bg-opacity-50">
-         
-                <Modal.Header className=" text-black "></Modal.Header>
-                
-                <Modal.Body className=" ">
-                  <div className="space-y-2 flex flex-col items-center justify-center text-center">
-                   
-                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                     Are you sure you want to Logout? 
-                    </p>
-                    <div className="flex space-x-8">
-                    <Link to='/'> <button className="bg-green-600 px-8 py-1 rounded-xl  ">Yes</button></Link>
-                   
-                    <button className="bg-red-700 px-8 py-1 rounded-xl " onClick={()=> setOpenModal(false)}>No</button>
-                   
-                    </div>
-                   
-                  </div>
-                </Modal.Body>
-
-                <Modal.Footer>
-                 
-                </Modal.Footer>
-                
-              </Modal>
-              </div>
               </button>
               <div >
               <Modal show={openModal} onClose={() => setOpenModal(false)} className=" pt-52 px-96 w-full h-screen flex items-center justify-center bg-gray-900 bg-opacity-50">
